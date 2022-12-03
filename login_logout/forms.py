e0daf4841ad99d409d278fb333cca112bbdb9ae2
ardhanidzaky <82014329+ardhanidzaky@users.noreply.github.com>
2022-12-03 10:11:20 +0700

    Finish

--- login_logout/forms.py
+++ login_logout/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
 from .models import AuthUserKapus
 
 class UserRegister(forms.ModelForm):
@@ -9,16 +10,29 @@ class UserRegister(forms.ModelForm):
             'email', 'hp', 'tipeUser'
         ]
 
-class UserLogin(forms.ModelForm):
-    class Meta:
-        model = AuthUserKapus
-        fields = [
-            'username', 'password'
-        ]
+class UserLogin(forms.Form):
+    username = forms.CharField(
+        max_length=50
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(),
+        max_length=50
+    )
 
-class UserChangeProfile(forms.ModelForm):
-    class Meta:
-        model = AuthUserKapus
-        fields = [
-            'password', 'lokasi', 'hp'
-        ]
+class UserChangeProfile(forms.Form):
+    password = forms.CharField(
+        widget=forms.PasswordInput(),
+        max_length=50,
+        initial='',
+        required=False
+    )
+    lokasi = forms.CharField(
+        max_length=50, 
+        initial='',
+        required=False
+    )
+    hp = forms.CharField(
+        max_length=50, 
+        initial='',
+        required=False
+    )

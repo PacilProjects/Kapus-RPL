80bd62b78d521f1f652e1f8649ee8d7606752a09
ardhanidzaky <82014329+ardhanidzaky@users.noreply.github.com>
2022-12-03 11:00:14 +0700

    Edit database settings

--- kapusrpl/settings.py
+++ kapusrpl/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'rlfdz3012',
+        'PASSWORD': 'admin',
         'HOST':'localhost',
         'PORT':5432,
         'OPTIONS':{

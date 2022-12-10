a882d7bb5a02bd631a13fd39910a827ee02df527
Ananda Fadhil <ananda.fadhil@ui.ac.id>
2022-12-10 19:33:34 +0700

    Fadhil BE 2

--- RatingPerpustakaan/urls.py
+++ RatingPerpustakaan/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import add_rating_suggestion, check_library, success
+from .views import add_rating_suggestion, check_library, success, failed
 
 urlpatterns = [
     path('add/<str:nama_perpus>', add_rating_suggestion, name='add_rating_suggestion'),
     path('check-library/', check_library, name='check_library'),
-    path('add/success/', success, name='success')
+    path('add/success/', success, name='success'),
+    path('add/failed/', failed, name='failed')
 ]
--- SaranPenambahanBuku/views.py
+++ SaranPenambahanBuku/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect, render
 from SaranPenambahanBuku.models import SaranPenambahanBuku
 from SaranPenambahanBuku.forms import PenambahanBukuForm
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 from django.core import serializers
 from django.http.response import HttpResponse
 # from booking.models import BookBorrow
@@ -12,7 +13,7 @@ def add_book_suggestion(request):
     add_book_suggestion = PenambahanBukuForm(request.POST or None)
     if (add_book_suggestion.is_valid() and request.method == 'POST'):
         add_book_suggestion.save()
-        return redirect('http://127.0.0.1:8000/saran-penambahan-buku/add/success/')
+        return redirect('/saran-penambahan-buku/add/success/')
     response = {'add_book_suggestion':add_book_suggestion}
     return render(request, 'saran_penambahan_buku.html', response)
 
